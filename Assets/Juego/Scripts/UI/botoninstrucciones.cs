f58949e174c7b9f69d3421d177f1606b1f178a99
LivingSwing <estebanpuerta2006@gmail.com>
2024-03-13 07:09:22 -0500

    Avance de la navegacion del proyecto

---
+++ Assets/Juego/Scripts/UI/botoninstrucciones.cs
@@ -0,0 +1,13 @@
+using MoreMountains.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botoninstrucciones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnclickBotoninstrucciones()
+    {
+        Debug.Log("Prueba");
+    }
+}
